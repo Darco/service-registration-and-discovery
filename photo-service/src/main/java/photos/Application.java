db6b3e493888b933dd55c49e800fbb6df0be2d9d
david <druizcmx@gmail.com>
2017-07-17 13:37:00 -0500

    Se realizan ajustes en configuracion del proyecto

--- photo-service/src/main/java/photos/Application.java
+++ photo-service/src/main/java/photos/Application.java
@@ -3,8 +3,6 @@ package photos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -13,12 +11,5 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-}
-
-@RestController
-@RequestMapping("/{userId}/photo")
-class PhotoRestController {
-
-
 }
 
---
+++ photo-service/src/main/java/photos/controller/PhotoRestController.java
@@ -0,0 +1,11 @@
+package photos.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/{userId}/photo")
+class PhotoRestController {
+
+
+}
